6b09103c882a17ff2dbeaf09c0839673b036f047
Casey Rodarmor <casey@rodarmor.com>
2024-11-13 13:38:50 -0800

    Add `schema` RPC

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -12,6 +12,7 @@
 #include <logging.h>
 #include <node/context.h>
 #include <node/kernel_notifications.h>
+#include <rpc/schema.h>
 #include <rpc/server_util.h>
 #include <rpc/util.h>
 #include <sync.h>
@@ -115,6 +116,12 @@ std::string CRPCTable::help(const std::string& strCommand, const JSONRPCRequest&
     return strRet;
 }
 
+UniValue CRPCTable::schema() const
+{
+    return CommandSchemas(this->mapCommands);
+}
+
+
 static RPCHelpMan help()
 {
     return RPCHelpMan{
@@ -140,6 +147,22 @@ static RPCHelpMan help()
     }
 
     return tableRPC.help(strCommand, jsonRequest);
+},
+    };
+}
+
+static RPCHelpMan schema()
+{
+    return RPCHelpMan{"schema",
+                "Return RPC command JSON Schema descriptions.\n",
+                {},
+                {
+                    RPCResult{RPCResult::Type::OBJ, "", "FOO"},
+                },
+                RPCExamples{""},
+        [&](const RPCHelpMan& self, const JSONRPCRequest& jsonRequest) -> UniValue
+{
+    return tableRPC.schema();
 },
     };
 }
@@ -241,6 +264,7 @@ static const CRPCCommand vRPCCommands[]{
     /* Overall control/query calls */
     {"control", &getrpcinfo},
     {"control", &help},
+    {"control", &schema},
     {"control", &stop},
     {"control", &uptime},
 };
